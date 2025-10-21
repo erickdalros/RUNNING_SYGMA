8787ed54bf77abc17c36aeff8813001ad7dfce02
erickdalros <erickhdros@gmail.com>
2025-10-21 20:04:16 -0300

    criação das pastas fundamentais para o projeto, adicionado o JAVAFX, SCENE BUILDER e JDBC

---
+++ src/main/java/application/Main.java
@@ -0,0 +1,24 @@
+package application;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) {
+        // carregar a cena inicial aqui
+    }
+
+    public static void main(String[] args) {
+
+        System.out.println("Inicializando application");
+
+        //launch(args);
+
+
+    }
+
+
+
+}
