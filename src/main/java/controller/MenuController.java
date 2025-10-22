964ef39b724a66152a3386dbb8812e8642abbf47
erickdalros <erickhdros@gmail.com>
2025-10-21 21:47:34 -0300

    apenas teste de aprendizado

---
+++ src/main/java/controller/MenuController.java
@@ -0,0 +1,20 @@
+package controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+
+public class MenuController {
+
+    @FXML
+    private Button Btn1;
+
+    @FXML
+    private void onBtn1Action() {
+
+        System.out.println("Click");
+
+    }
+
+
+}
